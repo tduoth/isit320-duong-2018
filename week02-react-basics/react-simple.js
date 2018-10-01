289d374d6bfa468d3f0c407a5d3231f69cb621e3
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-10-01 23:12:05 +0000

    i

--- week02-react-basics/react-simple.js
+++
@@ -1,8 +0,0 @@
- import React from 'react';
-
-  export default class ReactBasics extends React.Component {
-     render() {
-         return <h1>An H1 element in a React Component</h1>;
-     }
-  }
-
