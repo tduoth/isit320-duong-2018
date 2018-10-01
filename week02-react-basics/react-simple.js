24294f19e989ba75f97e81d7d6cd6962e08d1a18
bcuser <tduoth@gmail.com>
2018-10-01 15:08:31 -0700

    i

---
+++ week02-react-basics/react-simple.js
@@ -0,0 +1,8 @@
+ import React from 'react';
+
+  export default class ReactBasics extends React.Component {
+     render() {
+         return <h1>An H1 element in a React Component</h1>;
+     }
+  }
+
