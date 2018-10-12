4b7cea839e1bc42e88d16a1e58c63e409bb248e4
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-10-12 05:21:43 +0000

    resttest

--- week5-resttest/client/src/App.test.js
+++ week5-resttest/client/src/App.test.js
@@ -1,3 +1,12 @@
+
+import elfDebugEnzyme from './ElfDebugEnzyme';
+
+import {shallow} from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
