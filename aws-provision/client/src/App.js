99e985685084194772d1df31a2311fa7857b61bf
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-10-16 01:34:57 +0000

    i

---
+++ aws-provision/client/src/App.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'whatwg-fetch';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            file: 'File name will be placed here.',
+            status: 'waiting for server'
+        };
+    }
+
+    queryServer = () => {
+        const that = this;
+        fetch('/api/foo')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+                that.setState(foo => (json));
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+            
+    };
+    
+    createEducate = () => {
+      const that = this;
+        fetch('/api/create-educate')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+                that.setState(foo => (json));
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    
+            
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to Thanh React</h2>
+                </div>
+
+                <p className="App-intro">
+                    State: {this.state.status} File: {this.state.file}
+                </p>
+                
+                <button onClick={this.queryServer}>Bar</button> 
+                
+                <p>
+                <button onClick={this.createEducate}>Create with AWS Educate Account</button>
+                <button onClick={this.createWithAwsStandardAccount}>Create with AWS Standard Account</button>
+                <button onClick={this.associateElasticIp}>Associate Elastic Ip</button>
+                </p>
+                <p>
+                <button onClick={this.copyGetStarted}>Copy the GetStarted Script</button>
+                <button onClick={this.runGetStarted}>Run the GetStarted Script</button>
+                <button onClick={this.removeKnownHost}>Remove from KnownHost</button>
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
+
---
+++ aws-provision/server/routes/api.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router(); 
+const getAwsInstanceParams = require('GetAwsInstanceParams');
+const createInstance =require('AssociateElasticIp');
+
+
+/* Set up a route called foo. */
+router.get('/foo', function(request, response) {
+    var message = { 'State': 'success', 'status': 'Bar', 'file': 'api.js' };
+    console.log('Foo called:\n' + JSON.stringify(message, null, 4));
+    response.send(message);
+});
+
+router.get('/create-educate', function(request, response){
+    const awsInstanceParams = getAwsInstanceParams.awsEducate();
+    createInstance(awsInstanceParams);
+    response.send({result: 'sucess'});
+   
+});
+
+
+module.exports = router;
+
