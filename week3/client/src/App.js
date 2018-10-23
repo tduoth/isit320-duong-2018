4e000a7386f48a804c91b66fd0789d02acf17992
Thanh Duong <tduoth@gmail.com>
2018-10-22 17:59:46 -0700

    i

---
+++ week3/client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+class App extends Component {
+
+constructor() {
+    super();
+    this.state = {
+        file: 'unknown',
+        status: 'waiting'
+    };
+}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+
+          </p>
+	<p className="App-intro">
+                state: {this.state.status}
+            </p>
+            <p className="App-intro">
+                file: {this.state.file}
+            </p>
+            <button onClick={this.bar}>Bar</button>
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default App;
