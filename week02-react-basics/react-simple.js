20d5dbe58b927202b355a60d19b16dcd0db399c3
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-10-23 06:24:30 +0000

    i

--- week02-react-basics/react-simple.js
+++ week02-react-basics/react-simple.js
@@ -1,24 +1,7 @@
-var ReactBasics = function (_React$Component) {
-   _inherits(ReactBasics, _React$Component);
+  import React from 'react';
 
-   function ReactBasics() {
-      _classCallCheck(this, ReactBasics);
-
-      return _possibleConstructorReturn(this,
-        (ReactBasics.__proto__ ||
-           Object.getPrototypeOf(ReactBasics)).apply(this, arguments));
-   }
-
-   _createClass(ReactBasics, [{
-      key: 'render',
-      value: function render() {
-         return _react2.default.createElement(
-            'h1',
-            null,
-            'An H1 element in a React Component'
-         );
-      }
-   }]);
-
-   return ReactBasics;
-}(_react2.default.Component);
+  export default class ReactBasics extends React.Component {
+     render() {
+         return <h1>An H1 element in a React Component</h1>;
+     }
+  }
