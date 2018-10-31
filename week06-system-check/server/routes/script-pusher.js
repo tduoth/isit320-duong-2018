076fa4e9f56e5ae2627482337d72945213597629
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-10-31 04:05:10 +0000

    i

---
+++ week06-system-check/server/routes/script-pusher.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const program = require('commander');
+
+/* GET home page. */
+
+router.get('/copy-script', function(request, response) {
+    'use strict';
+    copyFile()
+        .then((result) => {
+            console.log(JSON.stringify(result, null, 4));
+            response.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            response.send(err);
+        })
+
+});
+
+
+
+
+module.exports = router;
