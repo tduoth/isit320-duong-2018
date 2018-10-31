1f112c3600dbda186cee08ec4e7f654048ffa957
Thanh Duong <tduoth@gmail.com>
2018-10-31 16:39:29 -0700

    I'

--- midterm/client/src/App.js
+++ midterm/client/src/App.js
@@ -5,23 +5,42 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      const radioWeb = (
+          <div className="container">
+          <form onSubmit={this.handleSubmit}>
+  <fieldset>
+      <div className="elf-form-field">
+
+          <legend>Services</legend>
+          <input
+      type="radio"
+      name="app-choice"
+      data-endpoint="0"
+      value="CpuInfo"
+      id="elf-radio-cpu"
+      onChange={this.handleChange}
+      />
+      <label htmlFor="elf-radio-cpu">CpuInfo</label>
+
+          <input
+      type="radio"
+      name="app-choice"
+      data-endpoint="0"
+      value="VersionCheck"
+      id="elf-radio-version"
+      onChange={this.handleChange}
+      />
+      <label htmlFor="elf-radio-version">Version Info</label>
+
       </div>
-    );
+
+      <div className="form-group">
+          <button type="submit" className="btn btn-primary">Run System Script</button>
+      </div>
+      </fieldset>
+      </form>
+      </div>
+  );
   }
 }
 
