bc77c3509aecccd0706239c4932e9460639a711c
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-02 08:12:30 +0000

    i

--- week07-system-check-refactor/client/src/App.js
+++ week07-system-check-refactor/client/src/App.js
@@ -1,57 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+    copyFile = () => {
+        const that = this;
+        fetch('/script-pusher/copy-file')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
 
-const radioWeb =  (
-      <div className="container">
-          <form onSubmit={this.handleSubmit} >
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
 
-              <div className="elf-form-field" >
-                  <input type="radio" name="app-choice" value="CpuInfo" id="elf-radio-cpu" onChange={this.handleChange}/>
-                  <label htmlFor="elf-radio-cpu">CpuInfo</label>
-
-                  <input type="radio" name="app-choice" value="VersionCheck" id="elf-radio-version" onChange={this.handleChange}/>
-                  <label htmlFor="elf-radio-version">Version Info</label>
-              </div>
-
-              <div className="form-group">
-                  <button type="submit" className="btn btn-primary">Run System Script</button>
-              </div>
-          </form>
-      </div>
-  );
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <main>
-    <section>
-{radioWeb}
-    </section>
-    <section>
-        <pre>{this.state.allData}</pre>
-    </section>
-    <button onClick={this.runFoo}>Run Foo</button>
-</main>
-
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+    render() {
+        return (
+            <div className="App">
+            <header>
+            <h1>Copy File </h1>
         </header>
-      </div>
+
+        <main>
+        <button onClick={this.copyFile}>Copy File</button>
+
+        </main>
+        <footer>
+        <p>&copy; by Thanh Duong </p>
+        </footer>
+        </div>
     );
-  }
+    }
 }
 
 export default App;
