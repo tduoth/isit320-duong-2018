e68d9c2201f4c858204908fcab23dd3f492ea52a
thanh duong <tduoth@gmail.com>
2018-11-01 21:19:43 -0700

    i

--- week5-ec2-copy-file/server/routes/script-pusher.js
+++ week5-ec2-copy-file/server/routes/script-pusher.js
@@ -1,11 +1,49 @@
 var express = require('express');
 var router = express.Router();
-const program = require('commander');
+const spawn = require('child_process').spawn;
 
-/* GET home page. */
+let allData = "";
 
-router.get('/copy-script', function(request, response) {
-    'use strict';
+const copyFile = () => {
+
+    return new Promise(function (resolve, reject) {
+
+        console.log("Copy to EC2", process.env.SETUP_LINUXBOX);
+
+        const pushScript = spawn('scp', [process.env.SETUP_LINUXBOX + '/CpuInfo', 'ec2-bc:/home/ubuntu']);
+
+        pushScript.stdout.on('data', (data) => {
+            console.log(`child stdout:\n${data}`);
+            allData += 'PUSH-SCRIPT: ' + data;
+            //console.log('PUSH', data);
+        });
+
+        pushScript.stderr.on('data', (data) => {
+            console.log(`child stderr:\n${data}`);
+            allData += 'PUSH-SCRIPT: ' + data;
+            //console.error('PUSH', data);
+        });
+
+        pushScript.on('close', (code) => {
+            resolve({
+                result: 'success',
+                code: code
+            });
+        });
+
+        pushScript.on('error', (code) => {
+            reject({
+                result: 'error',
+                code: code
+            });
+        });
+    });
+};
+
+router.get('/copy-file', (request, response) => {
+    //const result = { result: 'success'};
+    //response.send(result);
+
     copyFile()
         .then((result) => {
             console.log(JSON.stringify(result, null, 4));
@@ -15,10 +53,7 @@ router.get('/copy-script', function(request, response) {
             console.log(err);
             response.send(err);
         })
-
 });
 
-
-
-
 module.exports = router;
+
