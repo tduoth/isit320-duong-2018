bd5f70df46c47341656cfaafc054c76095c475a7
thanh duong <tduoth@gmail.com>
2018-11-01 21:19:25 -0700

    i

--- week5-ec2-copy-file/client/src/App.js
+++ week5-ec2-copy-file/client/src/App.js
@@ -1,56 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
-import 'whatwg-fetch';
 
 class App extends Component {
-
-
-constructor(props) {
-super(props);
-this.state = 
-{ allData: "unknown",
-  };
-}
-
-copyFile = () => {
-
-       const that= this;
-    fetch('/script-pusher/copy-file')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(json) {
-            console.log('parsed json', json.allData);
-	that.setState({allData:json.allData});   
-        })
-        .catch(function(ex) {
-            console.log('parsing failed', ex);
-        });
-};
-
-	
+  
+  copyFile = () => {
+        //const that = this;
+        fetch('/script-pusher/copy-file')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+                //that.setState(foo => (json));
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+  
   render() {
     return (
-
-
-     <div className="App">
-    <header>
-        <h1>Copy File</h1>
-    <p className="App-intro">
-                    Result: {this.state.allData}
-                </p>
-    </header>
-    <main>
-
-           <button onClick={this.copyFile}>Copy</button>
-    </main>
-</div>
-
+      <div className="App">
+        <header>
+          <h1>Copy File</h1>
+        </header>
+        <main>
+          <button onClick={this.copyFile}>Copy File</button>
+        </main>
+        <footer>
+          <p>&copy; by Duong Thanh</p>
+        </footer>
+      </div>
     );
   }
 }
 
-
-
 export default App;
-
