3886263fa523a449a06f87c16f3cb390c88a7ed7
thanh duong <tduoth@gmail.com>
2018-11-01 23:26:42 -0700

    i

--- week06-run-ssh/client/src/App.js
+++ week06-run-ssh/client/src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            allData: 'unknown'
+        };
+    }
+    callCpuInfo = () => {
+        const that = this;
+        fetch('ssh-runner/call-cpu-info')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                console.log('parsed json', json.allData);
+                that.setState({allData: json.allData});
+            })
+            .catch(function (ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            })
+    };
+
   render() {
+
+
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <header>
+          <p className="byline">Welcome To Thanh SSH Script</p>
+        <h1>Run SSH</h1>
+      </header>
+        <main>
+            <button onClick={this.callCpuInfo}>Call Cpu Info</button>
+            <p>{this.state.allData}</p>
+        </main>
+
+          <footer>
+              <p>&copy; 2018 </p>
+          </footer>
+
+
+
+
       </div>
     );
   }
