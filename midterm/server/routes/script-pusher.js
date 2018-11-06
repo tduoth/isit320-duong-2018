5ee3fc17e1c18da772f92b150f104bab59421458
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-06 00:06:47 +0000

    I

--- midterm/server/routes/script-pusher.js
+++ midterm/server/routes/script-pusher.js
@@ -1,40 +1,39 @@
+/* eslint-disable semi */
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-
 const spawn = require('child_process').spawn;
 
-let allData = "";
+let allData = '';
+let currentVersion = '';
 
 const copyFile = () => {
+    return new Promise(function(resolve, reject) {
+        console.log('Run CPU Info', process.env.SETUP_LINUXBOX);
 
-    return new Promise(function (resolve, reject) {
+        const pushScript = spawn(process.env.SETUP_LINUXBOX + '/CpuInfo');
 
-        console.log("Copy to EC2", process.env.SETUP_LINUXBOX);
-
-        const pushScript = spawn('scp', [process.env.SETUP_LINUXBOX + '/CpuInfo', 'ec2-bc:/home/ubuntu']);
-
-        pushScript.stdout.on('data', (data) => {
+        pushScript.stdout.on('data', data => {
             console.log(`child stdout:\n${data}`);
             allData += 'PUSH-SCRIPT: ' + data;
             //console.log('PUSH', data);
         });
 
-        pushScript.stderr.on('data', (data) => {
+        pushScript.stderr.on('data', data => {
             console.log(`child stderr:\n${data}`);
             allData += 'PUSH-SCRIPT: ' + data;
             //console.error('PUSH', data);
         });
 
-        pushScript.on('close', (code) => {
+        pushScript.on('close', code => {
             resolve({
                 result: 'success',
+                allData: allData,
                 code: code
             });
         });
 
-        pushScript.on('error', (code) => {
+        pushScript.on('error', code => {
             reject({
                 result: 'error',
                 code: code
@@ -43,18 +42,70 @@ const copyFile = () => {
     });
 };
 
-router.get('/copy-file', function(request, response) { 'use strict';
-    //response.send(Result: 'success'});
+const versionChk = () => {
+    return new Promise(function(resolve, reject) {
+        console.log('Run Version check', process.env.SETUP_LINUXBOX);
 
+        const pushScript = spawn(process.env.SETUP_LINUXBOX + '/VersionCheck');
+
+        pushScript.stdout.on('data', data => {
+            console.log(`child stdout:\n${data}`);
+            currentVersion += ' ' + data;
+            //console.log('PUSH', data);
+        });
+
+        pushScript.stderr.on('data', data => {
+            console.log(`child stderr:\n${data}`);
+            currentVersion += ' ' + data;
+            //console.error('PUSH', data);
+        });
+
+        pushScript.on('close', code => {
+            resolve({
+                result: 'success',
+                currentVersion: currentVersion,
+                code: code
+            });
+        });
+
+        pushScript.on('error', code => {
+            reject({
+                result: 'error',
+                code: code
+            });
+        });
+    });
+};
+
+router.get('/copy-file', function(request, response) {
+    'use strict';
     copyFile()
-        .then((result) => {
-            console.log("This is from the server: " + JSON.stringify(result, null, 4));
+        .then(result => {
+            console.log(JSON.stringify(result, null, 4));
             response.send(result);
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err);
             response.send(err);
+        });
+});
+
+router.get('/version-check', function(request, response) {
+    'use strict';
+    versionChk()
+        .then(result => {
+            console.log(JSON.stringify(result, null, 4));
+            response.send(result);
         })
+        .catch(err => {
+            console.log(err);
+            response.send(err);
+        });
+});
+
+router.get('/copy-script', function(request, response) {
+    'use strict';
+    response.send({ result: 'success' });
 });
 
 module.exports = router;
