ba0fe5b8eaef6a9aa21ab70cef86f491601730e6
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-08 00:33:34 +0000

    i

--- midterm/server/routes/script-pusher.js
+++ midterm/server/routes/script-pusher.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
 const spawn = require('child_process').spawn;
 
 let allData = '';
@@ -42,6 +45,44 @@ const copyFile = () => {
     });
 };
 
+
+const hostAddress = '52.32.226.69';
+
+const runCpuInfo = (hostAddress, response) => {
+    var conn = new Client();
+    conn.on('ready', function() {
+        console.log('Client :: ready');
+        conn.exec('~/CpuInfo', function(err, stream) {
+            if (err) throw err;
+            stream
+                .on('close', function(code, signal) {
+                    console.log(
+                        'Stream :: close :: code: ' +
+                            code +
+                            ', signal: ' +
+                            signal
+                    );
+                    conn.end();
+                    response.send({ result: 'success', allData: allData });
+                })
+                .on('data', function(data) {
+                    console.log('STDOUT: ' + data);
+                    allData += data;
+                })
+                .stderr.on('data', function(data) {
+                    console.log('STDERR: ' + data);
+                    allData += data;
+                });
+        });
+    }).connect({
+        host: hostAddress,
+        port: 22,
+        username: 'ubuntu',
+        privateKey: require('fs').readFileSync(
+            process.env.HOME + '/.ssh/ec2key'
+        )
+    });
+};
 
 const check = (request, response, next) => {
     console.log('REQUEST CHECK CALLED', request.query);
