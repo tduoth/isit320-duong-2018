1960ae800f1508f6194faee6edc89521b9f26c22
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-08 00:36:52 +0000

    i

--- midterm/server/routes/index.js
+++ midterm/server/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const Client = require('ssh2').Client;
 
 /* GET home page. */
 router.get('/', function(req, res, next) { 'use strict';
