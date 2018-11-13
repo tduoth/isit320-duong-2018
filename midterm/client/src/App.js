c2be7d8b6199b0a32dff2dac235ed4ad7f9a8869
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-13 06:43:04 +0000

    i

--- midterm/client/src/App.js
+++ midterm/client/src/App.js
@@ -197,6 +197,7 @@ handleSubmit = (event) => {
         
         return (
             <div className="App">
+            <ElfHeade/>
                 <header className="App-header">
                   <img src={logo} className="App-logo" alt="logo"/>
                   
