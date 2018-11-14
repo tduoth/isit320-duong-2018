aa6b05315516ccbad3d02ff8aba49562a2d2c173
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-14 23:37:36 +0000

    i

--- week08-radiocheck/server/app.js
+++ week08-radiocheck/server/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var scriptPusher = require('./routes/script-pusher');
+var sshRunner = require('./routes/ssh-runner');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/script-pusher', scriptPusher);
+app.use('/ssh-runner', sshRunner);
 
 
 // catch 404 and forward to error handler
