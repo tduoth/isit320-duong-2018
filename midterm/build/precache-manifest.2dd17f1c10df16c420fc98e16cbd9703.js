97ff7ab207b3a7fdbb1af0321909bf4984d23749
Thanh Duong <tduoth@gmail.com>
2018-11-19 16:41:20 -0800

    i

---
+++ midterm/build/precache-manifest.2dd17f1c10df16c420fc98e16cbd9703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e004b5b37f5cea3c804",
+    "url": "/static/js/main.4e004b5b.chunk.js"
+  },
+  {
+    "revision": "45af52f1181cf8191db9",
+    "url": "/static/js/1.45af52f1.chunk.js"
+  },
+  {
+    "revision": "4e004b5b37f5cea3c804",
+    "url": "/static/css/main.7de1ec67.chunk.css"
+  },
+  {
+    "revision": "06de38e4c22a84cec702488ead29b79f",
+    "url": "/index.html"
+  }
+];
