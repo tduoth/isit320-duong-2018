0d19354ecfb81cf71d871d8bac4ccee696c48b44
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-23 07:04:11 +0000

    i

--- midterm/server/routes/ssh-runner.js
+++ midterm/server/routes/ssh-runner.js
@@ -3,9 +3,36 @@ var router = express.Router();
 const Client = require('ssh2').Client;
 const elfUtils = require('elven-code').elfUtils;
 
-const hostAddress = '52.32.223.69';
+const hostAddress = '18.215.138.128';
+const identityFile = 'ec2-320-inclass';
+
 
 let allData = '';
+
+const getSshIp = () => {
+    return new Promise(function (resolve, reject) {
+        elfUtils.readFile(process.env.HOME + '/.ssh/config')
+            .then((content) => {
+                //var pattern = new RegExp('Host ec2-bc[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)');
+                var pattern = new RegExp('Host ec2-bc\n\t(.*)\n\t(.*)\n\t(.*)\n\t(.*)');
+                const result = {};
+                const match = content.result.match(pattern);
+                for (let i = 1; i < 5; i++) {
+                    if (match[i].startsWith('HostName')) {
+                        var hostPattern = new RegExp('HostName\\s(.*)');
+                        result.hostName = match[i].match(hostPattern)[1];
+                    }
+                    if (match[i].startsWith('IdentityFile')) {
+                        const idPattern = new RegExp('IdentityFile\\s(.*)');
+                        const path = match[i].match(idPattern)[1];
+                        result.identityFile = path.substring(path.lastIndexOf('/') + 1, path.length)
+                    }
+                }
+                resolve(result);
+            })
+            .catch(reject);
+    });
+};
 
 const runUptime = (hostAddress, response) => {
     var conn = new Client();
@@ -38,16 +65,21 @@ const runUptime = (hostAddress, response) => {
         port: 22,
         username: 'ubuntu',
         privateKey: require('fs').readFileSync(
-            process.env.HOME + '/.ssh/ec2key'
+            process.env.HOME + '/.ssh/ElfWest.pem'
         )
     });
 };
 
 
+router.get('/uptime', function(request, response) {
+    console.log('run-get-started called in ssh-runner', hostAddress);
+    getSshIp()
+        .then((result) => {
+            runUptime(result.hostName, result.identityFile, response);
+        })
+        .catch((err) => {
+            response.send(err);
+        });
 
-router.get('/run-uptime', function(request, response) {
-    allData = '';
-    runUptime(hostAddress, response);
 });
-
 module.exports = router;
