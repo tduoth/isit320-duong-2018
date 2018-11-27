3e35af2d657471b070297c0a86c09a12dce9ebf1
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-27 07:37:07 +0000

    i

--- week09-system-check-refactor/client/src/index.js
+++ week09-system-check-refactor/client/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import RadioLocal from './RadioLocal';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-ReactDOM.render(<RadioLocal/>, document.getElementById('radiolocal'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
