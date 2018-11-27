df1ed6d0c54dcb1a2e4718abd84fa0d93e8b805e
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-27 07:01:04 +0000

    i

--- week09-system-check-refactor/client/src/RadioLocal.js
+++ week09-system-check-refactor/client/src/RadioLocal.js
@@ -2,81 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.dataEndPoints = [
-        '/script-pusher/run-script?script=', 
-        '/script-pusher/run-system-tool?script=',  
-        '/script-pusher/get-host-name?script=',
-        '/script-pusher/run-uptime-tool?script='
-        ];
-        
-        this.state = {
-            allData:'',
-            selectedValue: '',
-            endPointIndex: 0,
-            
-        };
-    }
-
-runScript = (path, script) => {
-    const that = this;
-    if (!script) {
-        return;
-    }
-    fetch(path + script)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (json) {
-            console.log('allData', json.allData);
-            console.log('result', json.result);
-            console.log('code', json.code);
-            console.log('error', json.error);
-            let info = '';
-            if (json.result === 'error') {
-                info = json.error;
-            } else if (script === 'CpuInfo') {
-                var regex1 = RegExp('model name.*', 'g');
-                let array1 = regex1.exec(json.allData);
-                while (array1 !== null) {
-                    info += array1[0] + '\n';
-                    console.log(`Found ${array1[0]}.`);
-                    array1 = regex1.exec(json.allData);
-                }
-            } else { info = json.allData;}
-            that.setState({allData: info});
-        })
-        .catch(function (ex) {
-            console.log('parsing failed, URL bad, network down, or similar', ex);
-        });
-};
-
-
-handleChange = (event) => {
-    const selectedValue = event.target.value;
-    const endPointIndex = event.target.getAttribute('data-endpoint');
-    console.log('HANDLE CHANGE', selectedValue);
-    this.setState({...this.state, selectedValue: selectedValue, endPointIndex: endPointIndex});
-
-};
-
-handleSubmit = (event) => {
-    this.setState({allData: ''});
-    console.log('A name was submitted: ', this.state);
-            //uptime console
-     if(this.state.selectedValue === 'uptime'){
-            console.log('Going to console');
-            event.preventDefault();
-        } 
-        else{ this.runScript(this.dataEndPoints[this.state.endPointIndex], this.state.selectedValue);
-        event.preventDefault();
-        }
-};
+class RadioLocal extends Component {
 
  render() {
-        const radioWeb = (
+        const radioLocal = (
             <div className="container">
                 <form onSubmit={this.handleSubmit}>
                 <fieldset>
@@ -137,20 +66,8 @@ handleSubmit = (event) => {
             </div>
             );
             
-                     return (
-                <div className="App">
-                    <main>
-                        <section>
-                            {radioWeb}
-                        </section>
-                        <section>
-                            <pre>{this.state.allData}</pre>
-                        </section>
-                    </main>
-                </div>
-            );
+    
         }
     }
-
 
 export default RadioLocal;
