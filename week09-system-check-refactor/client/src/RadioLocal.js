1aefd6bbc4c629897b0b9fdbc6bd0f1484be7999
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-27 07:26:09 +0000

    i

--- week09-system-check-refactor/client/src/RadioLocal.js
+++ week09-system-check-refactor/client/src/RadioLocal.js
@@ -3,6 +3,23 @@ import './App.css';
 
 
 class RadioLocal extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.dataEndPoints = [
+        '/script-pusher/run-script?script=', 
+        '/script-pusher/run-system-tool?script=',  
+        '/script-pusher/get-host-name?script=',
+        '/script-pusher/run-uptime-tool?script=',
+         '/ssh-runner/run-script?script=',
+        '/ssh-runner/run-system-tool?script='
+        ];
+        
+        this.state = {
+            allData:'',
+            
+        };
+    }
 
  render() {
  return(
