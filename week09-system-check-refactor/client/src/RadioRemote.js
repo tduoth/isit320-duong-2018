a5620b020d5d170623210a2b5649bb6be1c755c7
Thanh duong <thanh.duong@bellevuecollege.edu>
2018-11-30 19:52:20 +0000

    i

--- week09-system-check-refactor/client/src/RadioRemote.js
+++ week09-system-check-refactor/client/src/RadioRemote.js
@@ -7,7 +7,7 @@ class RadioRemote extends Component {
     constructor(props) {
         super(props);
         this.dataEndPoints = [
-        '/ssh-runner/runCpuInfo', 
+        '/ssh-runner/', 
         '/ssh-runner/call-cpu-info?script=',  
         '/ssh-runner/get-host-name?script=',
         '/ssh-runner/uptime?script='
