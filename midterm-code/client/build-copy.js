979cf5c905303988318fa81b03734a469716da85
thanh duong <tduoth@gmail.com>
2018-12-01 20:09:38 -0800

    i

---
+++ midterm-code/client/build-copy.js
@@ -0,0 +1,29 @@
+
+> client@0.1.0 build /home/bcuser/isit320-duong-2018/midterm-code/client
+> react-scripts build "="
+
+Creating an optimized production build...
+Compiled successfully.
+
+File sizes after gzip:
+
+  34.76 KB  build/static/js/1.45af52f1.chunk.js
+  1.61 KB   build/static/js/main.f0e889c1.chunk.js
+  763 B     build/static/js/runtime~main.229c360f.js
+  459 B     build/static/css/main.f532cb43.chunk.css
+
+The project was built assuming it is hosted at the server root.
+You can control this with the homepage field in your package.json.
+For example, add this to build it for GitHub Pages:
+
+  "homepage" : "http://myname.github.io/myapp",
+
+The build folder is ready to be deployed.
+You may serve it with a static server:
+
+  serve -s build
+
+Find out more about deployment here:
+
+  http://bit.ly/CRA-deploy
+
